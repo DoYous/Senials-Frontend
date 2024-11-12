1972d48094e20037668f9f0596b25a45a839ac7c
min-g126 <qoalstj01@gmail.com>
2024-11-13 00:43:59 +0900

    매칭 게시판 일정 참여 멤버 조회 틀 css 추가

--- senials_frontend/src/App.js
+++ senials_frontend/src/App.js
@@ -5,6 +5,10 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import PartyMember from "./pages/party/partyMember";
 import PartyMeet from "./pages/party/partyMeet";
 import PartyReview from "./pages/party/partyReview";
+import MypageModify from "./pages/mypage/mypageModify";
+import MypageCalender from "./pages/mypage/mypageCalender";
+import MypageLike from "./pages/mypage/mypageLike";
+import Mypage from "./pages/mypage/mypage";
 
 function App() {
   return (
@@ -16,6 +20,18 @@ function App() {
           <Route path="/partyMeet" element={<PartyMeet />} />
             {/*매칭게시판 상세 모임 후기 작성하기*/}
             <Route path="/partyReview" element={<PartyReview />} />
+
+            {/*마이페이지 회원정보 변경*/}
+            <Route path="/mypageModify" element={<MypageModify />} />
+            {/*마이페이지+캘린더*/}
+            <Route path="/mypageCalender" element={<MypageCalender />} />
+            {/*취미 관심사 설정*/}
+            <Route path="/mypageLike" element={<MypageLike />} />
+            {/*사용자 프로필(남보기용)*/}
+            <Route path="/mypage" element={<Mypage />} />
+
+            {/*모임 전체 멤버 관리*/}
+
         </Routes>
       </BrowserRouter>
   );
--- senials_frontend/src/pages/party/partyMember.js
+++ senials_frontend/src/pages/party/partyMember.js
@@ -1,32 +1,38 @@
 import styles from './partyMember.module.css';
 import React, { useState, useEffect } from "react";
+/*리액트 아이콘 사용*/
+import { FaSearch } from "react-icons/fa";
+import { FaBell } from "react-icons/fa";
+import { FaAngleLeft } from "react-icons/fa6";
 
 function PartyMember() {
     return (
         <div className={styles.bigDiv}>
             <div className={styles.smallDiv}>
                 <div className={styles.navDiv}>
-                    <div className={styles.flexDiv}>
-                        <h1>모임명 -</h1>
+                    <div className={`${styles.flexDiv} ${styles.bigName}`}>
+                        <FaAngleLeft/>
+                        <h1 className={styles.marginLeft}>모임명 -</h1>
                         <h1>일정명</h1>
                     </div>
-                    <div className={styles.flexDiv}>
+                    <div className={`${styles.flexDiv} ${styles.searchDiv}`}>
                         <input type="text" placeholder="닉네임 검색"/>
-                        <p>아이콘</p>
+                        <button className={styles.iconDiv}><FaSearch /></button>
                     </div>
                 </div>
                 <hr className={styles.divHr}/>
                 <div className={styles.bigUserDiv}>
                     <div className={styles.userDiv}>
                         <div className={styles.flexDiv2}>
-                            <p className={styles.userProfileDiv}>사용자 프로필</p>
+                            <p className={styles.userProfileDiv}>프로필</p>
                             <p className={styles.userNameDiv}>사용자 이름</p>
                         </div>
-                        <button type="submit" className={styles.flexDiv}>
-                            <p>아이콘</p>신고
+                        <button type="submit" className={`${styles.flexDiv} ${styles.reportDiv}`}>
+                            <FaBell/>신고
                         </button>
                     </div>
                 </div>
+                <hr className={styles.divHr}/>
             </div>
         </div>
     );
