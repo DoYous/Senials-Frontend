8b421419ce45ceb86137ff09b3d0a7dcae74e3dc
min-g126 <qoalstj01@gmail.com>
2024-11-13 00:52:10 +0900

    매칭 게시판 상세 일정 추가 틀 css 추가

--- senials_frontend/src/pages/party/partyMeet.js
+++ senials_frontend/src/pages/party/partyMeet.js
@@ -6,39 +6,49 @@ function PartyMeet() {
         <div className={styles.bigDiv}>
             <div className={styles.smallDiv}>
         <form>
-            <h1>일정을 추가해주세요!</h1>
+            <h1 className={styles.nameflexDiv}><div className={styles.pink}>일정</div>을 추가해주세요!</h1>
             <div className={styles.bigSelectDiv}>
 
                 <div className={styles.selectDiv}>
                     <div className={styles.flexDiv}>
                         <p><label htmlFor="meetStartDate">시작 일정</label></p>
+                        <div className={styles.inputDiv}>
                         <input type="date" id="meetStartDate" name="meetStartDate" />
-                        <input type="time" id="meetStartTime" name="meetStartTime" />
+                        <input type="time" id="meetStartTime" name="meetStartTime" className={styles.marginLeft}/>
+                    </div>
                     </div>
                     <div className={styles.flexDiv}>
                         <p><label htmlFor="meetEndDate">종료 일정</label></p>
+                        <div className={styles.inputDiv}>
                         <input type="date" id="meetEndDate" name="meetEndDate" />
-                        <input type="time" id="meetEndTime" name="meetEndTime" />
+                        <input type="time" id="meetEndTime" name="meetEndTime" className={styles.marginLeft}/>
+                        </div>
                     </div>
 
                     <div className={styles.flexDiv}>
                     <p>모집인원</p>
-                    <input type="number" placeholder="모집인원을 정해주세요!" />
+                        <div className={styles.inputDiv}>
+                            <input type="number" placeholder="모집인원을 정해주세요!" />
+                        </div>
                     </div>
 
                     <div className={styles.flexDiv}>
                     <p>참가비용</p>
-                    <input type="number" placeholder="참가비용을 정해주세요!" />
+                        <div className={styles.inputDiv}>
+                            <input type="number" placeholder="참가비용을 정해주세요!" />
+                        </div>
                     </div>
 
                     <div className={styles.flexDiv}>
                     <p>위치</p>
-                    <input type="text" placeholder="정확한 위치를 작성해주세요!" />
+                        <div className={styles.inputDiv}>
+                            <input type="text" placeholder="정확한 위치를 작성해주세요!" />
+                        </div>
+                        </div>
+                    <div className={styles.btnDiv}>
+                        <button className={`${styles.commonBtn} ${styles.marginLeftAuto}`}>취소</button>
+                        <button className={`${styles.importantBtn} ${styles.marginLeft}`}>제출</button>
                     </div>
-                <div className={styles.btnDiv}>
-                    <button>취소</button>
-                    <button type={"submit"}>제출</button>
-                </div>
                 </div>
             </div>
         </form>
