0c44cbd797a7dbf7556a17004b182b8f06f5ad96
do0ik <oulwind00@naver.com>
2024-11-14 01:57:53 +0900

    [Issue #5] 모임 목록 기본 레이아웃 설계 1. react-bootstrap 라이브러리 설치 2. React 프로젝트 기본 생성 파일 제거 (App.test.js, logo.svg, reportWebVitals.js, setupTests.js)

--- senials_frontend/src/layouts/Layout.js
+++ senials_frontend/src/layouts/Layout.js
@@ -2,9 +2,9 @@ import { Outlet } from 'react-router-dom';
 
 function Layout() {
   return (
-    <>
+    <div style={{backgroundColor: 'grey', height: '100%'}}>
       <Outlet />
-    </>
+    </div>
   );
 }
 
