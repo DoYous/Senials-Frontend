70ed04807f8bf77708a4c2dbb01f5cbc3fd633de
do0ik <oulwind00@naver.com>
2024-11-18 19:20:33 +0900

    [Issue #11] 통합검색 결과 기본 레이아웃 설계 1. react-bootstrap 라이브러리 설치 2. React 프로젝트 기본 생성 파일 제거 (App.test.js, logo.svg, reportWebVitals.js, setupTests.js) 3. Layout 최상위 div css - height 수정, 배경색 삭제 4. 모듈css centerContainer - padding 수정, 그림자 효과 추가

--- senials_frontend/src/layouts/Layout.js
+++ senials_frontend/src/layouts/Layout.js
@@ -2,9 +2,10 @@ import { Outlet } from 'react-router-dom';
 
 function Layout() {
   return (
-    <>
+    // 레이아웃 최대높이 (사이트 최대높이 브라우저 창 크기)
+    <div style={{height: '100vh'}}>
       <Outlet />
-    </>
+    </div>
   );
 }
 
