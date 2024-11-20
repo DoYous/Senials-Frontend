1a52d3068288860c65806c2c872fbb450c8c50c1
GTU9 <107167326+GTU9@users.noreply.github.com>
2024-11-21 06:54:10 +0900

    샘플 프론트엔트 구성 #3

--- senials_frontend/src/pages/hobby/hobby-board-post.js
+++ senials_frontend/src/pages/hobby/hobby-board-post.js
@@ -1,44 +1,94 @@
-import React from 'react'
-import "./hobby-board-post.css"
-function HobbyBoardPost(){
-    return(
+import React from 'react';
+import styles from './hobby-board-post.module.css';
+
+let percentage=70;
+
+function HobbyBoardPost() {
+    return (
         <>
-         <div className='title'>인기 TOP3</div>
-            <div className='top3_list'>
-                <div className='top3'>
-                    <img src='' className='top3_img'></img>
-                    <div className='top3_name'>농구</div>
+            <div className={styles.title}>👑 <span style={{color:"#FF5391"}}>인기</span> TOP3</div>
+            <div className={styles.top3_list}>
+                <div className={styles.top3}>
+                    <img src='/img/sampleImg3.png' className={styles.top3_img} alt="농구" />
+                    <div className={styles.top3_name}>농구</div>
+                    <div>선호도 : {percentage}%</div>
+
+                    <div className={styles.progressBarContainer}>
+                        <div 
+                            className={styles.progressBar} 
+                            style={{ width: `${percentage}%` }} 
+                        ></div>
+                    </div>
+
                 </div>
-                <div className='top3'>
-                    <img src='' className='top3_img'></img>
-                    <div className='top3_name'>축구</div>
+                <div className={styles.top3}>
+                    <img src='/img/sampleImg3.png' className={styles.top3_img} alt="축구" />
+                    <div className={styles.top3_name}>축구</div>
+                    <div>선호도 :{percentage}%</div>
+
+                    <div className={styles.progressBarContainer}>
+                        <div 
+                            className={styles.progressBar} 
+                            style={{ width: `${percentage}%` }} 
+                        ></div>
+                    </div>
+
                 </div>
-                <div className='top3'>
-                    <img src='' className='top3_img'></img>
-                    <div className='top3_name'>낚시</div>
+                <div className={styles.top3}>
+                    <img src='/img/sampleImg3.png' className={styles.top3_img} alt="낚시" />
+                    <div className={styles.top3_name}>낚시</div>
+                    <div>선호도 : {percentage}%</div>
+
+                    <div className={styles.progressBarContainer}>
+                        <div 
+                            className={styles.progressBar} 
+                            style={{ width: `${percentage}%` }} 
+                        ></div>
+                    </div>
+
                 </div>
             </div>
-            <hr/>
-            <div className='hobby_list'>
-                <img src='' className='hobby_img'></img>
+            <hr />
+            <form className={styles.searchBox}>
+                <input type="text"/>
+                <button type="submit" className={styles.searchButton}>🔍</button>
+            </form>
+            <div className={styles.hobby_list}>
+                <img src='/img/sampleImg3.png' className={styles.hobby_img} alt="농구" />
                 <div>
-                    <div className='hobby_name'>농구</div>
-                    <div className='hobby_detail'>농구는 손으로 하는 게임입니다. 어렵지 않아요~</div>
+                    <div className={styles.hobby_name}>농구</div>
+
+                    <div>선호도 : {percentage}%</div>
+                    <div className={styles.progressBarContainer}>
+                        <div 
+                            className={styles.progressBar} 
+                            style={{ width: `${percentage}%` }} 
+                        ></div>
+                    </div>
+
+                    <div className={styles.hobby_detail}>농구는 손으로 하는 게임입니다. 어렵지 않아요~</div>
                 </div>
-                
             </div>
 
-            <div className='hobby_list'>
-                <img src='' className='hobby_img'></img>
+            <div className={styles.hobby_list}>
+                <img src='/img/sampleImg3.png' className={styles.hobby_img} alt="축구" />
                 <div>
-                    <div className='hobby_name'>축구</div>
-                    <div className='hobby_detail'>축구는 발로 하는 게임입니다. 어렵지 않아요~</div>
+                    <div className={styles.hobby_name}>축구</div>
+
+                    <div>선호도 : {percentage}%</div>
+                    <div className={styles.progressBarContainer}>
+                        <div 
+                            className={styles.progressBar} 
+                            style={{ width: `${percentage}%` }} 
+                        ></div>
+                    </div>
+                    <div className={styles.hobby_detail}>축구는 발로 하는 게임입니다. 어렵지 않아요~</div>
                 </div>
             </div>
 
-            <button className='suggest_hobby_button'>취미 추가 건의</button>
+            <button className={styles.suggest_hobby_button}>취미 추가 건의</button>
         </>
     );
 }
 
-export default HobbyBoardPost
+export default HobbyBoardPost;
