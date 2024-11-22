90d64091e2390a94c43a53dbcb26890648b4480f
GTU9 <107167326+GTU9@users.noreply.github.com>
2024-11-22 23:05:43 +0900

    샘플 프론트 구성 #4

--- senials_frontend/src/pages/hobby/hobby-detail-post.js
+++ senials_frontend/src/pages/hobby/hobby-detail-post.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import styles from './hobby-detail-post.module.css';
 
-
-
 function HobbyDetailPost() {
     return (
-        <>
+        <div className={styles.body}>
             <div className={styles.hobby}>
                 <img src='/img/sampleImg4.png' className={styles.hobby_img} alt="취미" />
                 <div className={styles.hobby_text}>
                     <div className={styles.hobby_name}>축구</div>
                     <div className={styles.hobby_detail}>공이란걸 발로 차는 운동</div>
-
                     <div className={styles.preference}>
                         <div>평균 선호도 : 80%</div>
                         <div className={styles.progressBarContainer}>
@@ -40,7 +37,8 @@ function HobbyDetailPost() {
                 </div>  
             </div>         
             <HobbyReview />
-        </>
+            <div/>
+        </div>
     );
 }
 
@@ -49,17 +47,34 @@ function HobbyReview() {
         <div className={styles.hobby_review}>
             <div className={styles.hobby_review_detail}>
                 <div className={styles.user_info}>
-                    <img src='' className={styles.user_img} alt="사용자" />
+                    <img src='/img/sampleImg5.png' className={styles.user_img} alt="사용자" />
                     <div className={styles.user_name}>홍길동</div>
-                    <div className={styles.review_point}>별점 * * * * *</div>
-                    <button className={styles.report_button}>신고</button>
+                    <div className={styles.review_point}>별점 
+                        <StarPoint/>
+                    </div>
+                    <button className={styles.report_button}><img src='/img/reportIcon.png' style={{width:'18px',height:'18px'}} alt=''></img>신고</button>
                 </div>
                 <div className={styles.review_title}>축구 사랑해요!!</div>
                 <div className={styles.review_detail}>아 축구 너무 재밌어요</div>
-                <img className={styles.review_img} alt="후기" />
+                <div className={styles.review_img_container}>
+                    <img src='/img/sampleImg4.png' className={styles.review_img} alt="후기" />
+                    <img src='/img/sampleImg4.png' className={styles.review_img} alt="후기" />
+                </div>
                 <button className={styles.update_review_button}>수정</button>
             </div>
         </div>
+    );
+}
+
+function StarPoint(){
+    return(
+        <span>
+            <img className={styles.star} src='/img/starFull.png' alt=''></img>
+            <img className={styles.star} src='/img/starFull.png' alt=''></img>
+            <img className={styles.star} src='/img/starFull.png' alt=''></img>
+            <img className={styles.star} src='/img/starFull.png' alt=''></img>
+            <img className={styles.star} src='/img/starEmpty.png' alt=''></img>
+        </span>
     );
 }
 
