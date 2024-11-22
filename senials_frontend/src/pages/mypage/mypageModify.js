aeb87248a5a4c62d43928419b0ab72a2f5bbd0da
min-g126 <qoalstj01@gmail.com>
2024-11-22 18:29:18 +0900

    common css 추가, 마이페이지 디자인 글자 크기 조정, 모임 리뷰 양식 페이지 글자 크기 조정

--- senials_frontend/src/pages/mypage/mypageModify.js
+++ senials_frontend/src/pages/mypage/mypageModify.js
@@ -1,4 +1,5 @@
 import styles from './mypageModify.module.css';
+import common from '../common/common.module.css';
 import React, { useState, useEffect } from "react";
 /*아이콘*/
 import {FaAngleLeft} from "react-icons/fa6";
@@ -12,7 +13,7 @@ function MypageModify() {
                 <div className={styles.modifyDiv}>
                     <div className={styles.bigName}>
                         <FaAngleLeft/>
-                        <h1 className={styles.nameflexDiv}>
+                        <h1 className={`${styles.nameflexDiv} ${common.firstFont}`}>
                             <div className={styles.pink}>회원정보 </div>
                              변경
                         </h1>
@@ -22,22 +23,22 @@ function MypageModify() {
                 <div className={styles.smallDiv}>
                     <div className={styles.mainDiv}>
                     <div className={styles.mainName}>
-                        <p>프로필 사진 변경</p>
+                        <p className={`${common.secondFont} ${styles.margin0}`}>프로필 사진 변경</p>
                         <button className={styles.profileChange}>
                         <FaArrowUpRightFromSquare/>
                         </button>
                     </div>
                     <hr className={styles.divHr}/>
                     <div className={styles.flexName}>
-                        <p>닉네임</p>
+                        <p className={`${common.secondFont} ${styles.margin0}`}>닉네임</p>
                         <div className={styles.flex2}>
                             <p className={styles.nickNameDiv}>기존 닉네임</p>
                             <button className={`${styles.commonBtn} ${styles.marginLeft}`}>변경</button>
                         </div>
                     </div>
                         <hr className={styles.divHr}/>
-                    <p>한줄 소개</p>
-                    <div className={styles.contentDiv}>기존 소개문</div>
+                    <p className={`${common.secondFont} ${styles.margin0}`}>한줄 소개</p>
+                    <div className={`${styles.contentDiv} ${common.secondFont}`}>기존 소개문</div>
                     <div className={styles.saveDiv}>
                         <button className={`${styles.commonBtn} ${styles.saveMargin}`}>저장</button>
                     </div>
--- senials_frontend/src/pages/party/partyReview.js
+++ senials_frontend/src/pages/party/partyReview.js
@@ -1,5 +1,6 @@
 /*매칭게시판 상세 모임 후기 작성*/
 import styles from './partyReview.module.css';
+import common from '../common/common.module.css';
 import React, { useState, useEffect } from "react";
 
 function PartyReview() {
@@ -7,14 +8,14 @@ function PartyReview() {
         <div className={styles.bigDiv}>
             <div className={styles.smallDiv}>
                 <form className={styles.formDiv}>
-                    <h1 className={styles.nameflexDiv}>
+                    <h1 className={`${styles.nameflexDiv} ${common.firstFont}`}>
                         <div className={styles.pink}>후기</div>
                         를 작성해주세요!
                     </h1>
                     <div className={styles.bigSelectDiv}>
                         <div className={styles.selectDiv}>
                             <div className={styles.flexDiv}>
-                                <p><label htmlFor="meetStartDate">최초가입일</label></p>
+                                <p className={common.secondFont}><label htmlFor="meetStartDate">최초가입일</label></p>
                                 <div className={styles.inputDiv}>
                                 <input type="date" id="meetStartDate" name="meetStartDate"  className={styles.inputStyle}/>
                                 <input type="time" id="meetStartTime" name="meetStartTime"  className={`${styles.marginLeft} ${styles.inputStyle}`}/>
@@ -22,7 +23,7 @@ function PartyReview() {
                                 </div>
 
                             <div className={styles.flexDiv}>
-                                <p>평점을 매겨주세요!</p>
+                                <p className={common.secondFont}>평점을 매겨주세요!</p>
                                 <div className={styles.rateInfo}>
                                     <span className={styles.filledStar}>★★★</span>
                                     <span className={styles.unfilledStar}>☆☆</span>
@@ -30,8 +31,11 @@ function PartyReview() {
                             </div>
 
                             <div className={styles.contentDiv}>
-                                <p>내용</p>
+                                <p className={common.secondFont}>내용</p>
                                 <textarea className={styles.textDiv} id="content" name="content" placeholder="활동이 어땠는지 당신의 이야기를 적어주세요!"></textarea>
+                            </div>
+                            <div className={styles.pDiv}>
+                                <p className={`${common.thirdFont} ${styles.pDiv2}`}>1000자 내외</p>
                             </div>
 
                             <div className={styles.btnDiv}>
