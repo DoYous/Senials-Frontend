5b4bec1b5e91c21481b2025f54f15196b97e9e17
min-g126 <qoalstj01@gmail.com>
2024-11-22 14:21:16 +0900

    body 태그 주석 처리, 클래스 명으로 꾸민 css 제외 하고 공통 태그 사용한 부분 삭제

--- senials_frontend/src/pages/party/partyMeet.js
+++ senials_frontend/src/pages/party/partyMeet.js
@@ -5,7 +5,7 @@ function PartyMeet() {
     return (
         <div className={styles.bigDiv}>
             <div className={styles.smallDiv}>
-        <form>
+        <form className={styles.formDiv}>
             <h1 className={styles.nameflexDiv}><div className={styles.pink}>일정</div>을 추가해주세요!</h1>
             <div className={styles.bigSelectDiv}>
 
@@ -13,36 +13,36 @@ function PartyMeet() {
                     <div className={styles.flexDiv}>
                         <p><label htmlFor="meetStartDate">시작 일정</label></p>
                         <div className={styles.inputDiv}>
-                        <input type="date" id="meetStartDate" name="meetStartDate" />
-                        <input type="time" id="meetStartTime" name="meetStartTime" className={styles.marginLeft}/>
+                        <input type="date" id="meetStartDate" name="meetStartDate" className={styles.inputStyle}/>
+                        <input type="time" id="meetStartTime" name="meetStartTime" className={`${styles.marginLeft} ${styles.inputStyle}`}/>
                     </div>
                     </div>
                     <div className={styles.flexDiv}>
                         <p><label htmlFor="meetEndDate">종료 일정</label></p>
                         <div className={styles.inputDiv}>
-                        <input type="date" id="meetEndDate" name="meetEndDate" />
-                        <input type="time" id="meetEndTime" name="meetEndTime" className={styles.marginLeft}/>
+                        <input type="date" id="meetEndDate" name="meetEndDate"  className={styles.inputStyle}/>
+                        <input type="time" id="meetEndTime" name="meetEndTime" className={`${styles.marginLeft} ${styles.inputStyle}`}/>
                         </div>
                     </div>
 
                     <div className={styles.flexDiv}>
                     <p>모집인원</p>
                         <div className={styles.inputDiv}>
-                            <input type="number" placeholder="모집인원을 정해주세요!" />
+                            <input type="number" placeholder="모집인원을 정해주세요!"  className={styles.inputStyle}/>
                         </div>
                     </div>
 
                     <div className={styles.flexDiv}>
                     <p>참가비용</p>
                         <div className={styles.inputDiv}>
-                            <input type="number" placeholder="참가비용을 정해주세요!" />
+                            <input type="number" placeholder="참가비용을 정해주세요!"  className={styles.inputStyle}/>
                         </div>
                     </div>
 
                     <div className={styles.flexDiv}>
                     <p>위치</p>
                         <div className={styles.inputDiv}>
-                            <input type="text" placeholder="정확한 위치를 작성해주세요!" />
+                            <input type="text" placeholder="정확한 위치를 작성해주세요!" className={styles.inputStyle} />
                         </div>
                         </div>
                     <div className={styles.btnDiv}>
--- senials_frontend/src/pages/party/partyReview.js
+++ senials_frontend/src/pages/party/partyReview.js
@@ -6,18 +6,18 @@ function PartyReview() {
     return (
         <div className={styles.bigDiv}>
             <div className={styles.smallDiv}>
-                <form>
+                <form className={styles.formDiv}>
                     <h1 className={styles.nameflexDiv}>
                         <div className={styles.pink}>후기</div>
-                        을 작성해주세요!
+                        를 작성해주세요!
                     </h1>
                     <div className={styles.bigSelectDiv}>
                         <div className={styles.selectDiv}>
                             <div className={styles.flexDiv}>
                                 <p><label htmlFor="meetStartDate">최초가입일</label></p>
                                 <div className={styles.inputDiv}>
-                                <input type="date" id="meetStartDate" name="meetStartDate"/>
-                                <input type="time" id="meetStartTime" name="meetStartTime"  className={styles.marginLeft}/>
+                                <input type="date" id="meetStartDate" name="meetStartDate"  className={styles.inputStyle}/>
+                                <input type="time" id="meetStartTime" name="meetStartTime"  className={`${styles.marginLeft} ${styles.inputStyle}`}/>
                                 </div>
                                 </div>
 
@@ -31,7 +31,7 @@ function PartyReview() {
 
                             <div className={styles.contentDiv}>
                                 <p>내용</p>
-                                <textarea id="content" name="content" placeholder="활동이 어땠는지 당신의 이야기를 적어주세요!"></textarea>
+                                <textarea className={styles.textDiv} id="content" name="content" placeholder="활동이 어땠는지 당신의 이야기를 적어주세요!"></textarea>
                             </div>
 
                             <div className={styles.btnDiv}>
