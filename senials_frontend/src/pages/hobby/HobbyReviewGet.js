d36cd38d0cd30073f7d325636b3542aca8e78d7a
GTU9 <107167326+GTU9@users.noreply.github.com>
2024-11-25 19:46:17 +0900

    클래스명 카멜케이스로 변경 #12

--- senials_frontend/src/pages/hobby/HobbyReviewGet.js
+++ senials_frontend/src/pages/hobby/HobbyReviewGet.js
@@ -3,53 +3,60 @@ import styles from "./HobbyReviewGet.module.css";
 
 function HobbyReviewGet() {
     return (
-        <div className={styles.body}>
-            <div className={styles.write_review}>
-                <div className={styles.title}><span style={{color: "#FF5391"}}>후기</span>를 적어주세요!</div>
-                <div className={styles.write_date}>작성일자 <spna style={{color: "#b3b3b3",fontWeight:"normal"}}>2024-10-07</spna></div>
-                <div className={styles.review_point}>
+        <div className={styles.page}>
+            <div className={styles.writeReview}>
+                <div className={styles.title}>
+                    <span style={{color: "#FF5391"}}>후기</span>를 적어주세요!
+                </div>
+                <div className={styles.writeDate}>
+                    작성일자 <span style={{color: "#b3b3b3", fontWeight: "normal"}}>2024-10-07</span>
+                </div>
+                <div className={styles.reviewPoint}>
                     <div className={styles.text}>평점을 매겨주세요!</div>
-                    <div className={styles.star_point}><StarPoint/></div>
+                    <div className={styles.starPoint}><StarPoint /></div>
                 </div>
 
-                <form className={styles.write_review_detail}>
+                <form className={styles.writeReviewDetail}>
                     <div className={styles.text}>후기 내용</div>
-                    <textarea className={styles.input_text} placeholder='후기를 적어주세요! 준비물, 마음가짐 등 자유롭게 기입해주세요!'/>
-                    <div className={styles.select_hobby_ability}>
+                    <textarea 
+                        className={styles.inputText} 
+                        placeholder="후기를 적어주세요! 준비물, 마음가짐 등 자유롭게 기입해주세요!" 
+                    />
+                    <div className={styles.selectHobbyAbility}>
                         <div className={styles.text}>몸이 불편하신 곳이 있을까요?</div>
-                        <input type="radio" id="ability-yes" name="hobby_ability" value="y" />
-                        <label htmlFor="ability-yes">예</label>
-                        <input type="radio" id="ability-no" name="hobby_ability" value="n" />
-                        <label htmlFor="ability-no">아니요</label>
+                        <input type="radio" id="abilityYes" name="hobbyAbility" value="y" />
+                        <label htmlFor="abilityYes">예</label>
+                        <input type="radio" id="abilityNo" name="hobbyAbility" value="n" />
+                        <label htmlFor="abilityNo">아니요</label>
                     </div>
-                    <div className={styles.select_hobby_tendency}>
-                        <div className={styles.text}>다신의 성향은 어떤가요?</div>
-                        <input type="radio" id="tendency-in" name="hobby_tendency" value="in" />
-                        <label htmlFor="tendency-in">내향적</label>
-                        <input type="radio" id="tendency-out" name="hobby_tendency" value="out" />
-                        <label htmlFor="tendency-out">외향적</label>
+                    <div className={styles.selectHobbyTendency}>
+                        <div className={styles.text}>당신의 성향은 어떤가요?</div>
+                        <input type="radio" id="tendencyIn" name="hobbyTendency" value="in" />
+                        <label htmlFor="tendencyIn">내향적</label>
+                        <input type="radio" id="tendencyOut" name="hobbyTendency" value="out" />
+                        <label htmlFor="tendencyOut">외향적</label>
                     </div>
-                    <div className={styles.select_hobby_budget}>
+                    <div className={styles.selectHobbyBudget}>
                         <div className={styles.text}>지출범위</div>
-                        <input type="radio" id="budget" name="hobby_budget" value="" />
+                        <input type="radio" id="budget" name="hobbyBudget" value="" />
                         <label htmlFor="budget">1~1000000000</label>
                     </div>
-                    <div className={styles.select_hobby_level}>
+                    <div className={styles.selectHobbyLevel}>
                         <div className={styles.text}>난이도</div>
-                        <input type="radio" id="level-1" name="hobby_level" value="1" />
-                        <label htmlFor="level-1">쉬움</label>
-                        <input type="radio" id="level-2" name="hobby_level" value="2" />
-                        <label htmlFor="level-2">좀 쉬움</label>
-                        <input type="radio" id="level-3" name="hobby_level" value="3" />
-                        <label htmlFor="level-3">평범</label>
-                        <input type="radio" id="level-4" name="hobby_level" value="4" />
-                        <label htmlFor="level-4">좀 어려움</label>
-                        <input type="radio" id="level-5" name="hobby_level" value="5" />
-                        <label htmlFor="level-5">어려움</label>
+                        <input type="radio" id="level1" name="hobbyLevel" value="1" />
+                        <label htmlFor="level1">쉬움</label>
+                        <input type="radio" id="level2" name="hobbyLevel" value="2" />
+                        <label htmlFor="level2">좀 쉬움</label>
+                        <input type="radio" id="level3" name="hobbyLevel" value="3" />
+                        <label htmlFor="level3">평범</label>
+                        <input type="radio" id="level4" name="hobbyLevel" value="4" />
+                        <label htmlFor="level4">좀 어려움</label>
+                        <input type="radio" id="level5" name="hobbyLevel" value="5" />
+                        <label htmlFor="level5">어려움</label>
                     </div>
-                    <div className={styles.button_container}>
+                    <div className={styles.buttonContainer}>
                         <button>취소</button>
-                        <input type='submit' value="제출" />
+                        <input type="submit" value="제출" />
                     </div>
                 </form>
             </div>
@@ -57,14 +64,14 @@ function HobbyReviewGet() {
     );
 }
 
-function StarPoint(){
-    return(
+function StarPoint() {
+    return (
         <span>
-            <img className={styles.star} src='/img/starFull.png' alt=''></img>
-            <img className={styles.star} src='/img/starFull.png' alt=''></img>
-            <img className={styles.star} src='/img/starFull.png' alt=''></img>
-            <img className={styles.star} src='/img/starFull.png' alt=''></img>
-            <img className={styles.star} src='/img/starEmpty.png' alt=''></img>
+            <img className={styles.star} src="/img/starFull.png" alt="" />
+            <img className={styles.star} src="/img/starFull.png" alt="" />
+            <img className={styles.star} src="/img/starFull.png" alt="" />
+            <img className={styles.star} src="/img/starFull.png" alt="" />
+            <img className={styles.star} src="/img/starEmpty.png" alt="" />
         </span>
     );
 }
