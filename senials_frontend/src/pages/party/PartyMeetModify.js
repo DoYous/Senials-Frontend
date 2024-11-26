16029ba3f11047dbbce00a104023b70d656e6e2b
min-g126 <qoalstj01@gmail.com>
2024-11-26 23:26:59 +0900

    모임 일정 수정, 모임 리뷰 수정 페이지 생성

---
+++ senials_frontend/src/pages/party/PartyMeetModify.js
@@ -0,0 +1,63 @@
+import styles from './PartyMeet.module.css';
+import common from '../common/Common.module.css';
+import React, { useState, useEffect } from "react";
+
+function PartyMeetModify() {
+    return (
+        <div className={styles.bigDiv}>
+            <div className={styles.smallDiv}>
+                <form className={styles.formDiv}>
+                    <h1 className={`${styles.nameflexDiv} ${common.firstFont}`}><div className={styles.pink}>일정</div>을 추가해주세요!</h1>
+                    <div className={styles.bigSelectDiv}>
+
+                        <div className={styles.selectDiv}>
+                            <div className={styles.flexDiv}>
+                                <h7 className={common.secondFont}><label htmlFor="meetStartDate">시작 일정</label></h7>
+                                <div className={styles.inputDiv}>
+                                    <input type="date" id="meetStartDate" name="meetStartDate" className={styles.inputStyle}/>
+                                    <input type="time" id="meetStartTime" name="meetStartTime" className={`${styles.marginLeft} ${styles.inputStyle}`}/>
+                                </div>
+                            </div>
+                            <div className={styles.flexDiv}>
+                                <h7  className={common.secondFont}><label htmlFor="meetEndDate">종료 일정</label></h7>
+                                <div className={styles.inputDiv}>
+                                    <input type="date" id="meetEndDate" name="meetEndDate"  className={styles.inputStyle}/>
+                                    <input type="time" id="meetEndTime" name="meetEndTime" className={`${styles.marginLeft} ${styles.inputStyle}`}/>
+                                </div>
+                            </div>
+
+                            <div className={styles.flexDiv}>
+                                <div className={common.secondFont}>모집인원</div>
+                                <div className={styles.inputDiv}>
+                                    <input type="number" placeholder="모집인원을 정해주세요!"  className={styles.inputStyle}/>
+                                </div>
+                            </div>
+
+                            <div className={styles.flexDiv}>
+                                <div className={common.secondFont}>참가비용</div>
+                                <div className={styles.inputDiv}>
+                                    <input type="number" placeholder="참가비용을 정해주세요!"  className={styles.inputStyle}/>
+                                </div>
+                            </div>
+
+                            <div className={styles.flexDiv}>
+                                <h7 className={common.secondFont}>위치</h7>
+                                <div className={styles.inputDiv}>
+                                    <input type="text" placeholder="정확한 위치를 작성해주세요!" className={styles.inputStyle} />
+                                </div>
+                            </div>
+                            <div className={styles.btnDiv}>
+                                <button className={`${common.commonBtn} ${styles.marginLeftAuto}`}>삭제</button>
+                                <button className={`${common.commonBtn} ${styles.marginLeft}`}>취소</button>
+                                <button className={`${common.importantBtn} ${styles.marginLeft}`}>제출</button>
+                            </div>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+
+    );
+}
+
+export default PartyMeetModify;
---
+++ senials_frontend/src/pages/party/PartyReviewModify.js
@@ -0,0 +1,72 @@
+/*매칭게시판 상세 모임 후기 수정*/
+import styles from './PartyReview.module.css';
+import common from '../common/Common.module.css';
+import React, { useState, useEffect } from "react";
+
+function PartyReviewModify() {
+    return (
+        <div className={styles.bigDiv}>
+            <div className={styles.smallDiv}>
+                <form className={styles.formDiv}>
+                    <h1 className={`${styles.nameflexDiv} ${common.firstFont}`}>
+                        <div className={styles.pink}>후기</div>
+                        를 작성해주세요!
+                    </h1>
+                    <div className={styles.bigSelectDiv}>
+                        <div className={styles.selectDiv}>
+                            <div className={styles.flexDiv}>
+                                <p className={common.secondFont}><label htmlFor="meetStartDate">최초가입일</label></p>
+                                <div className={styles.inputDiv}>
+                                    <input type="date" id="meetStartDate" name="meetStartDate"  className={styles.inputStyle}/>
+                                    <input type="time" id="meetStartTime" name="meetStartTime"  className={`${styles.marginLeft} ${styles.inputStyle}`}/>
+                                </div>
+                            </div>
+
+                            <div className={styles.flexDiv}>
+                                <p className={common.secondFont}>평점을 매겨주세요!</p>
+                                <Star />
+                            </div>
+
+                            <div className={styles.contentDiv}>
+                                <p className={common.secondFont}>내용</p>
+                                <textarea className={styles.textDiv} id="content" name="content" placeholder="활동이 어땠는지 당신의 이야기를 적어주세요!"></textarea>
+                            </div>
+                            <div className={styles.pDiv}>
+                                <p className={`${common.thirdFont} ${styles.pDiv2}`}>1000자 내외</p>
+                            </div>
+
+                            <div className={styles.btnDiv}>
+                                <button className={`${common.commonBtn} ${styles.marginLeftAuto}`}>삭제</button>
+                                <button className={`${common.commonBtn} ${styles.marginLeft}`}>취소</button>
+                                <button className={`${styles.importantBtn} ${styles.marginLeft}`}>제출</button>
+                            </div>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+
+    );
+}
+
+/* 별점 매기기 */
+function Star() {
+    const [rating, setRating] = useState(0); /* 현재 별점 상태 */
+
+    const handleStarClick = (index) => {
+        setRating(index); /*클릭*/
+    };
+
+    return (
+        <div className={styles.rateInfo}>
+            {[1, 2, 3, 4, 5].map((star) => (
+                <div
+                    key={star}
+                    className={`${styles.baseStar} ${star <= rating ? styles.filledStar : ""}`}
+                    onClick={() => handleStarClick(star)}
+                />
+            ))}
+        </div>
+    );
+}
+export default PartyReviewModify;
