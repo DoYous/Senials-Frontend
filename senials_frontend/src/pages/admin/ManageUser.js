01bab883a43bc27537f6e85b5aa4784fd36c7bb2
GTU9 <107167326+GTU9@users.noreply.github.com>
2024-12-06 01:18:26 +0900

    useSelector, useDispatch 임포트 예시

--- senials_frontend/src/pages/admin/ManageUser.js
+++ senials_frontend/src/pages/admin/ManageUser.js
@@ -2,14 +2,18 @@ import React,{useEffect, useState} from 'react';
 import styles from './admin.module.css';
 import {useDispatch,userSelector, useSelector} from "react-redux";
 
-let state=useSelector((state)=>state)
-let dispatch=useDispatch()
+
 
 
 let userData={id:'sangik', name:'김상익',birth:'1999-09-09', email:'sangik9999@naver.com',gender:'male',reportCount:12}
 
 
 function ManageUser(){
+
+    let state=useSelector((state)=>state)
+    let dispatch=useDispatch()
+
+
     return(
         <div>
             <div className={styles.adminHeader}>
