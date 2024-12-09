88f4390d623bb067b93a2b685e7c9ede6e1beab1
Ahnhyojun <hyojunahn111@naver.com>
2024-12-10 00:20:13 +0900

    #55 아이디 비번 입력 폼 추가

--- senials_frontend/src/pages/login/login.js
+++ senials_frontend/src/pages/login/login.js
@@ -1,17 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 // CSS
 import styles from './login.module.css';
 
 function Login() {
     const navigate = useNavigate();
+    const [userId, setUserId] = useState('');
+    const [password, setPassword] = useState('');
 
     const linkSignup = () => {
         navigate('/login/signup');
     };
 
     const handleKakaoLogin = () => {
-        window.location.href = "/oauth2/authorization/kakao"; // 카카오 로그인 URL로 이동
+        window.location.href = "/login/oauth2/authorization/kakao"; // 카카오 로그인 URL로 이동
+    };
+
+    const handleLogin = () => {
+        // 여기에서 로그인 로직을 추가합니다.
+        console.log('아이디:', userId);
+        console.log('비밀번호:', password);
+        // 로그인 요청을 서버에 보내는 로직을 추가할 수 있습니다.
     };
 
     return (
@@ -21,6 +30,27 @@ function Login() {
                 <img className={styles.kakaobt} src={'/image/kakaologinbt.png'} alt="카카오 로그인" onClick={handleKakaoLogin} />
             </div>
             <div className={styles.OrgLogin}>일반로그인</div>
+
+            <div className={styles.OrginputContainer}>
+                <input
+                    type="text"
+                    placeholder="아이디"
+                    value={userId}
+                    onChange={(e) => setUserId(e.target.value)}
+                    className={styles.OrginputField}
+                />
+                <input
+                    type="password"
+                    placeholder="비밀번호"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    className={styles.OrginputField}
+                />
+                <button className={styles.OrgloginButton} onClick={handleLogin}>
+                    확인
+                </button>
+            </div>
+
             <div className={styles.buttonContainer}>
                 <button className={styles.simpleSignupButton} onClick={linkSignup}>
                     일반 회원가입
