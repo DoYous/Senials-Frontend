4ccd4e6ddce1f15ba133b695d6de613d660b5de4
Ahnhyojun <hyojunahn111@naver.com>
2024-12-13 01:04:35 +0900

    #111 토큰 값을 제대로 받게 변경, 백앤드로 유저 넘버 보내는법 추가

--- senials_frontend/src/pages/login/success.js
+++ senials_frontend/src/pages/login/success.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { jwtDecode } from 'jwt-decode';
+import axios from 'axios';
 
 function Success() {
     const [userInfo, setUserInfo] = useState(null);
@@ -28,19 +29,44 @@ function Success() {
         navigate('/login'); // 로그인 페이지로 리다이렉트
     };
 
+    //백엔드로 유저넘버 보내는법
+    const sendUserNumber = async () => {
+        if (userInfo && userInfo.userNumber) {
+            try {
+                const response = await axios.post('/sendUserNumber', { //보내고 싶은 get 주소
+                    userNumber: userInfo.userNumber,
+                }, {
+                    headers: {
+                        Authorization: `Bearer ${localStorage.getItem("token")}` // 인증 토큰 추가
+                    }
+                });
+
+                console.log('서버 응답:', response.data);
+            } catch (error) {
+                console.error('서버에 데이터 전송 실패:', error);
+            }
+        } else {
+            console.error('사용자 번호가 없습니다.');
+        }
+    };
+
     return (
         <div>
             <h2>인증 성공!</h2>
             {userInfo ? (
                 <div>
                     <h3>사용자 정보:</h3>
-                    <p>이름: {userInfo.sub || '이름 없음'}</p>
+                    <p>이름: {userInfo.nickname || '이름 없음'}</p>
                     <p>이메일: {userInfo.email || '이메일 없음'}</p>
-                    <p>성별: {userInfo.gender || '성별 없음'}</p>
+                    <p>성별: {userInfo.gender === 0 ? '남성' : userInfo.gender === 1 ? '여성' : '알 수 없음'}</p>
                     <p>권한: {userInfo.roles ? userInfo.roles.join(', ') : '없음'}</p>
-                    <p>상태: {userInfo.status || '상태 없음'}</p>
-                    <p>프로필 사진: {userInfo.profile_picture ?
-                        <img src={userInfo.profile_picture} alt="프로필"/> : '사진 없음'}</p>
+                    <p>상태: {userInfo.userStatus || '상태 없음'}</p>
+                    <p>프로필 사진: {userInfo.profileImg ?
+                        <img src={userInfo.profileImg} alt="프로필"/> : '사진 없음'}</p>
+                    <p>가입 날짜: {userInfo.signupDate || '가입 날짜 없음'}</p>
+                    <p>사용자 번호: {userInfo.userNumber || '번호 없음'}</p>
+                    <p>신고 카운트: {userInfo.reportCount || '카운트 없음'}</p>
+                    <p>사용자 UUID: {userInfo.userUuid || 'UUID 없음'}</p>
                     <button onClick={handleLogout}>로그아웃</button>
                 </div>
             ) : (
