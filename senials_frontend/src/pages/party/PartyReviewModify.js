1055f3508ca28de6c72d1915745a51bca27143cf
min-g126 <qoalstj01@gmail.com>
2024-12-13 18:00:47 +0900

    모임 일정 수정 및 모임 리뷰 수정

--- senials_frontend/src/pages/party/PartyReviewModify.js
+++ senials_frontend/src/pages/party/PartyReviewModify.js
@@ -1,140 +1,191 @@
-import React, { useState } from "react";
+import styles from './PartyReview.module.css';
+import common from '../common/Common.module.css';
+import React, { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
-import axios from "axios";
-import styles from "./PartyReview.module.css";
-import common from "../common/Common.module.css";
+import axios from 'axios';
 
-function PartyReview() {
-  const navigate = useNavigate();
-  const { partyNumber } = useParams(); // URL에서 partyNumber 가져오기
-  const [review, setReview] = useState({
-    meetStartDate: "",
-    meetStartTime: "",
-    rating: 0,
-    content: "",
-  });
+function PartyReviewModify() {
+    const navigate = useNavigate();
+    const { partyNumber } = useParams(); // URL에서 따오기
+    const { partyReviewNumber } = useParams();
+    const [meetStartDate, setMeetStartDate] = useState(""); // 시작 날짜
+    const [meetStartTime, setMeetStartTime] = useState(""); // 시작 시간
+    const [rating, setRating] = useState(0);                // 별점
+    const [content, setContent] = useState("");             // 내용
 
-  const handleInputChange = (e) => {
-    const { name, value } = e.target;
-    setReview({ ...review, [name]: value });
-  };
+    // 정보 가져오기
+ useEffect(() => {
+    const fetchMeetData = async () => {
+        try {
+            const response = await axios.get(`/partyboards/${partyNumber}/partyreviews/${partyReviewNumber}`);
+            const meetData = response.data.results;
 
-  const handleStarClick = (index) => {
-    setReview({ ...review, rating: index });
-  };
+            if (!meetData) {
+                throw new Error("데이터가 존재하지 않습니다.");
+            }
 
-  const handleCancel = (e) => {
-    e.preventDefault();
-    navigate(-1);
-  };
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
-    const partyReviewData = {
-      partyBoardNumber: parseInt(partyNumber, 10),
-      partyReviewRate: review.rating,
-      partyReviewDetail: review.content,
+            setMeetStartDate(meetData.startDate || "");
+            setMeetStartTime(meetData.startTime || "");
+            setRating(meetData.rating || 0);
+            setContent(meetData.content || "");
+        } catch (error) {
+            console.error("API 호출 에러:", error.response ? error.response.data : error.message);
+        }
     };
 
-    try {
-      const response = await axios.post(
-        `/partyboards/${partyNumber}/partyreviews`,
-        partyReviewData
-      );
-      if (response.status === 200) {
-        alert("제출되었습니다!");
-        navigate(-1);
-      }
-    } catch (error) {
-      console.error("후기 제출 실패:", error);
-      alert("후기 제출 중 오류가 발생했습니다.");
-    }
-  };
+    fetchMeetData();
+}, []); // 의존성 배열에 아무 값도 넣지 않아 한번만 호출되게 함.
 
-  return (
-    <div className={styles.bigDiv}>
-      <div className={styles.smallDiv}>
-        <form className={styles.formDiv} onSubmit={handleSubmit}>
-          <h1 className={`${styles.nameflexDiv} ${common.firstFont}`}>
-            <div className={styles.pink}>후기</div>를 작성해주세요!
-          </h1>
-          <div className={styles.bigSelectDiv}>
-            <div className={styles.selectDiv}>
-              <div className={styles.flexDiv}>
-                <p className={common.secondFont}>
-                  <label htmlFor="meetStartDate">최초가입일</label>
-                </p>
-                <div className={styles.inputDiv}>
-                  <input
-                    type="date"
-                    id="meetStartDate"
-                    name="meetStartDate"
-                    className={styles.inputStyle}
-                    value={review.meetStartDate}
-                    onChange={handleInputChange}
-                  />
-                  <input
-                    type="time"
-                    id="meetStartTime"
-                    name="meetStartTime"
-                    className={`${styles.marginLeft} ${styles.inputStyle}`}
-                    value={review.meetStartTime}
-                    onChange={handleInputChange}
-                  />
-                </div>
-              </div>
-              <div className={styles.flexDiv}>
-                <p className={common.secondFont}>평점을 매겨주세요!</p>
-                <div className={styles.rateInfo}>
-                  {[1, 2, 3, 4, 5].map((star) => (
-                    <div
-                      key={star}
-                      className={`${styles.baseStar} ${
-                        star <= review.rating ? styles.filledStar : ""
-                      }`}
-                      onClick={() => handleStarClick(star)}
-                    />
-                  ))}
-                </div>
-              </div>
-              <div className={styles.contentDiv}>
-                <p className={common.secondFont}>내용</p>
-                <textarea
-                  className={styles.textDiv}
-                  id="content"
-                  name="content"
-                  placeholder="활동이 어땠는지 당신의 이야기를 적어주세요!"
-                  value={review.content}
-                  onChange={handleInputChange}
-                ></textarea>
-              </div>
-              <div className={styles.pDiv}>
-                <p className={`${common.thirdFont} ${styles.pDiv2}`}>
-                  1000자 내외
-                </p>
-              </div>
-              <div className={styles.btnDiv}>
-                <button
-                  type="button"
-                  className={`${common.commonBtn} ${styles.marginLeftAuto}`}
-                  onClick={handleCancel}
-                >
-                  취소
-                </button>
-                <button
-                  type="submit"
-                  className={`${styles.importantBtn} ${styles.marginLeft}`}
-                >
-                  제출
-                </button>
-              </div>
+
+    // 제출 (수정)
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        try {
+            const response =  await axios.put(`/partyboards/${partyNumber}/partyreviews/${partyReviewNumber}`,{
+                partyReviewRate: rating,
+                partyReviewDetail: content,
+                partyReviewWriteDate: `${meetStartDate}T${meetStartTime}`,
+            });
+            alert("수정되었습니다!");
+            navigate(-1); // 이전 페이지로 이동
+        } catch (error) {
+            console.error("수정 중 에러 발생:", error.response ? error.response.data : error.message);
+            alert("수정에 실패했습니다. 다시 시도해주세요.");
+        }
+    };
+
+    // 삭제
+    const handleRemove = async (event) => {
+        event.preventDefault();
+        const confirmDelete = window.confirm("정말 삭제하시겠습니까?");
+        if (confirmDelete) {
+            try {
+                await axios.delete(`/partyboards/${partyNumber}/partyreviews/${partyReviewNumber}`);
+
+                alert("삭제되었습니다!");
+                navigate(-1); // 이전 페이지로 이동
+            } catch (error) {
+                console.error("삭제 중 에러 발생:", error.response ? error.response.data : error.message);
+                alert("삭제에 실패했습니다. 다시 시도해주세요.");
+            }
+        }
+    };
+
+    // 취소
+    const handleCancel = (event) => {
+        event.preventDefault();
+        navigate(-1); // 이전 페이지로 이동
+    };
+
+    return (
+        <div className={styles.bigDiv}>
+            <div className={styles.smallDiv}>
+                <form className={styles.formDiv} onSubmit={handleSubmit}> {/* 폼 제출용 */}
+                    <h1 className={`${styles.nameflexDiv} ${common.firstFont}`}>
+                        <div className={styles.pink}>후기</div>
+                        를 작성해주세요!
+                    </h1>
+                    <div className={styles.bigSelectDiv}>
+                        <div className={styles.selectDiv}>
+                            <div className={styles.flexDiv}>
+                                <p className={common.secondFont}><label htmlFor="meetStartDate">최초 가입일</label></p>
+                                <div className={styles.inputDiv}>
+                                    <input
+                                        type="date"
+                                        id="meetStartDate"
+                                        name="meetStartDate"
+                                        className={styles.inputStyle}
+                                        value={meetStartDate}
+                                        onChange={(e) => setMeetStartDate(e.target.value)}
+                                    />
+                                    <input
+                                        type="time"
+                                        id="meetStartTime"
+                                        name="meetStartTime"
+                                        className={`${styles.marginLeft} ${styles.inputStyle}`}
+                                        value={meetStartTime}
+                                        onChange={(e) => setMeetStartTime(e.target.value)}
+                                    />
+                                </div>
+                            </div>
+
+                            <div className={styles.flexDiv}>
+                                <p className={common.secondFont}>평점을 매겨주세요!</p>
+                                <Star rating={rating} setRating={setRating} />
+                            </div>
+
+                            <div className={styles.contentDiv}>
+                                <p className={common.secondFont}>내용</p>
+                                <textarea
+                                    className={styles.textDiv}
+                                    id="content"
+                                    name="content"
+                                    placeholder="활동이 어땠는지 당신의 이야기를 적어주세요!"
+                                    value={content}
+                                    onChange={(e) => setContent(e.target.value)}
+                                ></textarea>
+                            </div>
+                            <div className={styles.pDiv}>
+                                <p className={`${common.thirdFont} ${styles.pDiv2}`}>1000자 내외</p>
+                            </div>
+
+                            <div className={styles.flex}>
+                                <button
+                                    type="button"
+                                    className={styles.uniqueBtn}
+                                    onClick={handleRemove}
+                                >
+                                    삭제
+                                </button>
+                                <div className={styles.flex2}>
+                                    <button
+                                        type="button"
+                                        className={common.commonBtn}
+                                        onClick={handleCancel}
+                                    >
+                                        취소
+                                    </button>
+                                    <button
+                                        type="submit"
+                                        className={`${styles.importantBtn} ${styles.marginLeft}`}
+                          
+                                    >
+                                        제출
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </form>
             </div>
-          </div>
-        </form>
-      </div>
-    </div>
-  );
+        </div>
+    );
 }
 
-export default PartyReview;
+/* 별점 매기기 */
+function Star({ rating, setRating }) {
+    const [selectedRating, setSelectedRating] = useState(0);
+
+    useEffect(() => {
+        setSelectedRating(rating); // 부모 컴포넌트에서 전달받은 값으로 초기화
+    }, [rating]);
+
+    const handleStarClick = (index) => {
+        setSelectedRating(index);
+        setRating(index);
+    };
+
+    return (
+        <div className={styles.rateInfo}>
+            {[1, 2, 3, 4, 5].map((star) => (
+                <div
+                    key={star}
+                    className={`${styles.baseStar} ${star <= selectedRating ? styles.filledStar : ""}`}
+                    onClick={() => handleStarClick(star)}
+                />
+            ))}
+        </div>
+    );
+}
+
+export default PartyReviewModify;
